e1422c83f44c9e7b5826286756dcf7e9b7ef8641
wlaub <goldcray@gmail.com>
2021-01-29 19:52:55 -0800

    some filters

--- message.py
+++ message.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 import tts
 
@@ -5,6 +7,47 @@ from pydub import AudioSegment
 from pydub.playback import play
 import pydub.effects as fx
 import pydub.playback as playback
+
+###################
+# Message Filters #
+###################
+
+class RegexReplace():
+    def __init__(self, re, replacement):
+        self.re = re
+        self.replacement = replacement
+    
+    def process(self, snippet):
+        if not isinstance(snippet, tts.SpeechSnippet): return False
+
+        pieces = re.split(self.re, snippet.data['text'])
+        if len(pieces) == 1: return False
+
+        result = []
+        for idx, piece in enumerate(pieces):
+            result.append(tts.SpeechSnippet({'text': piece}, snippet.config))
+            if idx < len(pieces) - 1:
+                result.append(self.replacement)
+
+        return result
+
+class TooLongTruncate():
+    def __init__(self, max_length):
+        self.max_length = max_length
+
+    def process(self, snippet):
+        if not isinstance(snippet, tts.SpeechSnippet): return False
+
+        otext = snippet.data['text']
+        pieces = otext.split(' ')
+        pieces = list(map(lambda x: x[:self.max_length], pieces))
+        text = ' '.join(pieces)
+        if text == otext: return False
+        result = [tts.SpeechSnippet({'text': text}, snippet.config)]
+
+        return result
+
+
 
 ########################
 # Twitch Chat Messages #
@@ -22,7 +65,12 @@ class Message():
 
     max_message_duration = 30 # in seconds
 
-    filters = []
+    filters = [
+        RegexReplace( # replaces urls
+            'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+            tts.SpeechSnippet({'text': 'URL Removed'})),
+        TooLongTruncate(30),
+    ]
 
     def __init__(self, msg, tags, history):
         """
@@ -72,6 +120,7 @@ class Message():
 
     def parse_emotes(self):
         emotes = self.tags['emotes']
+        if emotes == None: return []
         result = []
         kinds = emotes.split('/')
         for kind in kinds:
@@ -140,7 +189,6 @@ class Message():
                 result.insert(0, tts.SpeechSnippet({'text': self.user.display_name}, {'max_length': 1}))
        
         return result
-
 
 
 #############
